1595ad46c2663826c700fe5e5a8977f9b33837e7
user <user@usernoMacBook-Pro.local>
2022-09-23 21:07:00 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import {getAllPokemon} from './utils/pokemon'
 
 function App() {
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
+  // 最初からuseEffectでデータを取ろうとしているのでloadingの初期値はtrueでいい
+  const [loading, setLoading] = useState(true)
+
+  // 第2引数が[]なのでマウントされたら発火(コンポーネントが生成されて、レンダリングされた際)
+  // つまり初回にレンダリングされた際に発火
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      // 全てのポケモンデーターを取得
+      let res = await getAllPokemon(initialURL);
+      console.log(res)
+      setLoading(false);
+    };
+    fetchPokemonData();
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loading ? (
+        <h1>ロード中・・・</h1>
+      ) : (
+        <>
+          <h1>ポケモンデータを取得しました</h1>
+        </>
+      )}
     </div>
   );
 }
