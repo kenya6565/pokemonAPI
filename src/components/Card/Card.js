7d2260b892c267bd7ddea672795fa32824e1c7e4
user <user@usernoMacBook-Pro.local>
2022-09-24 22:04:13 +0900

    add CSS

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,8 +1,42 @@
 import React from 'react'
+import "./Card.css"
 
 const Card = ({ pokemon }) => {
   return (
-    <div>Card</div>
+    <div className='card'>
+      <div className='cardImg'>
+        <img alt="" src={pokemon.sprites.front_default} />
+      </div>
+      <h3 className="cardName">{pokemon.name}</h3>
+      <div className="cardTypes">
+        <div>タイプ</div>
+        {pokemon.types.map((type) => {
+          return (
+            <div>
+              <span className="typeName">{type.type.name}</span>
+            </div>
+          )
+        })}
+      </div>
+      <div className="cardInfo">
+        <div className="cardData">
+          <p className="title">
+            重さ:{pokemon.weight}
+          </p>
+        </div>
+        <div className="cardData">
+          <p className="title">
+            高さ:{pokemon.height}
+          </p>
+        </div>
+        <div className="cardData">
+          <p className="title">
+            アビリティ:{pokemon.abilities[0].ability.name}
+          </p>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
